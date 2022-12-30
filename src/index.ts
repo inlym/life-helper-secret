a9a9ffaef3d3d09a6d4bea73ecff115080d9737d
inlym <inlym@qq.com>
2022-12-30 20:40:05 +0800

    :sparkles: 完成基本功能

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello')
+/** 应用配置 */
+export interface AppConfig {
+  key: string
+  secret: string
+}
+
+/** 获取应用配置 */
+export function getAppConfig(): AppConfig {
+  return {
+    key: 'xxxxxxxxxxxx',
+    secret: 'xxxxxxxxxxxx',
+  }
+}
